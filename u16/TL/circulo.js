80c5b839d6d00b210f6ecb7a1fdc8f8afa62ef32
minh <minhnghiemquangminh75@gmail.com>
2022-10-27 10:51:09 +0700

    .

---
+++ u16/TL/circulo.js
@@ -0,0 +1,13 @@
+let Circle = function (radius) {
+    this.radius = radius;
+    this.getRadius = function () {
+        return radius;
+    }
+    this.getArea = function () {
+        return Math.PI * radius * radius;
+    }
+};
+let circle = new Circle(4);
+let radius = circle.getRadius()
+let area = circle.getArea()
+console.log("radius: " + radius + "; area: " + area);
